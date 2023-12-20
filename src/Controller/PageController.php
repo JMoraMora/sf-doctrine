16cb0e75231ff13153d176f1bc58634935749753
Jaison Mora <jasonjairmoramora@gmail.com>
2023-12-20 00:12:53 -0500

    vista para producto individual

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Product;
+use App\Entity\Tag;
 use Doctrine\ORM\EntityManagerInterface;
 
 class PageController extends AbstractController
@@ -16,6 +17,22 @@ class PageController extends AbstractController
     {
         return $this->render('page/home.html.twig', [
             'products' => $em->getRepository(Product::class)->findAll(),
+        ]);
+    }
+
+    #[Route('/tag/{id}', name: 'app_tag')]
+    public function tag(Tag $tag): Response
+    {
+        return $this->render('page/tag.html.twig', [
+            'tag' => $tag,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_product')]
+    public function product(Product $product): Response
+    {
+        return $this->render('page/product.html.twig', [
+            'product' => $product,
         ]);
     }
 }
